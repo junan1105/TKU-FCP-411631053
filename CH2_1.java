ed12487923bf18b32cc6a840f8f8c4a4523f3ffa
junan1105 <113399134+junan1105@users.noreply.github.com>
2024-07-02 11:50:26 +0800

    我的第一支java程式

---
+++ CH2_1.java
@@ -0,0 +1,15 @@
+// ch_2_1, //定義類別 ch2_1
+public class CH2_1 {  //定義public類別ch2_1
+    public static void main(String[] args) {  //main()函數，主程式開始
+        int num1=2;  //宣告整數 num
+        int num2=3;  //宣告整數 num
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cats are trnning"); //印出字串及變數內容
+        System.out.println(num2+"dogs are trnning"); //印出字串及變數內容
+        System.out.println(num3+"birds are trnning"); //印出字串及變數內容
+        System.out.println(num4+"tigers are trnning"); //印出字串及變數內容
+
+    } 
+
+}
