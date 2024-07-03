2e8daeacb2f0b898bba4fb139ddbde50cfd7b3b7
junan1105 <113399134+junan1105@users.noreply.github.com>
2024-07-03 11:31:45 +0800

    我的第一支java程式

---
+++ ch2_6.java
@@ -0,0 +1,11 @@
+// Ch2_6, 沒有縮排的程式碼
+public class ch2_6 { //定義  
+    public static void main(String[] args){
+        int a=2;  //宣告整數變數a，並設值為2
+        int b=3;  //宣告整數變數b，並設值為3
+        int c=a+b; //宣告整數變數c，並設值為a+b
+             
+        System.out.println("a+b="+c);
+    }
+    
+}
