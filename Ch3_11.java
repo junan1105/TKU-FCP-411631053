8e2485cb6078adc421dc3907919d10c5357e7dc5
楊竣安411631053 <113399134+junan1105@users.noreply.github.com>
2024-07-08 14:07:15 +0800

    我的第一支java程式

---
+++ Ch3_11.java
@@ -0,0 +1,10 @@
+// Ch3_11
+public class Ch3_11 {   
+    public static void main(String[] args){
+
+        boolean status=false;
+        System.out.println("status="+status);
+
+    }
+    
+}
---
+++ Ch3_13.java
@@ -0,0 +1,17 @@
+// Ch3_13
+import java.util.Scanner;
+public class Ch3_13 {   
+    public static void main(String[] args){
+
+        Scanner scn=new Scanner(System.in);
+
+        System.out.print("What's your name? ");
+        String name=scn.next();
+        System.out.print("How old are you? ");
+        int age=scn.nextInt();
+        System.out.print(name+",  "+age+"  years old.");
+        scn.close();
+
+    }
+
+}
